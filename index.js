94cc337af78d3009ad4792a3c40adc76d3f12ae9
Dulmandakh <dulmandakh@gmail.com>
2017-07-24 00:52:51 +0800

    import firebase-app.js and firebase-messaging.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var fbTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'ember-cli-firebase-messaging'
+  name: 'ember-cli-firebase-messaging',
+   included() {
+    this._super.included.apply(this, arguments);
+    this.import('vendor/firebase-app.js');
+    this.import('vendor/firebase-messaging.js');
+  },
+  treeForVendor(vendorTree) {
+    this._super.treeForVendor.apply(this, arguments);
+    var trees = [];
+    var modulePath = path.dirname(require.resolve('firebase'));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      fbTransform(new Funnel(modulePath))
+    );
+    return mergeTrees(trees);
+  },
 };
